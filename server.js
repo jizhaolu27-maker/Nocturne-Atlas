82c3ad413af22d691a9a6625328e72ed89a691a7
Churis (ChuFhus) <161580645+ChuFhus@users.noreply.github.com>
2026-08-01 18:12:03 +0000

    fix(server): wire memory archive path

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ const {
   getStoryWorkspaceDir,
   getStoryMemoryFile,
   getStoryMemoryChunkFile,
+  getStoryMemoryArchiveFile,
   getStoryKnowledgeChunkFile,
   getStoryKnowledgeEmbeddingFile,
   getStoryProposalFile,
